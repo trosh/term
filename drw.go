d8f03fdf7f297bd2d7f47830393d1d36be6ec53f
trosh <jg.trosh@gmail.com>
2013-02-17 17:13:39 +0100

    added scr type

--- drw.go
+++ drw.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 	"math"
-	"strings"
 )
 
 type pxl struct {
@@ -12,14 +11,37 @@ type pxl struct {
 	y int
 }
 
-func Plot(p pxl, bg int, size pxl) {
-	if p.x >= 1 && p.x <= size.x &&
-	p.y >= 1 && p.y <= size.y {
+type scr struct {
+	p1 pxl
+	p2 pxl
+	bg int
+}
+
+func (s scr) Size() pxl {
+	size := pxl{s.p2.x - s.p1.x, s.p2.y - s.p1.x}
+	if s.p2.x < s.p1.x { size.x = s.p1.x - s.p2.x }
+	if s.p2.y < s.p1.y { size.y = s.p1.y - s.p2.y }
+	return size
+}
+
+func (s scr) Flush() {
+	fmt.Printf("\033[48;5;%dm", s.bg)
+	for y := s.p1.y; y <= s.p2.y; y += 1 {
+		fmt.Printf("\033[%d;%dH", y, s.p1.x)
+		for x := s.p1.x; x <= s.p2.x; x += 1 {
+			fmt.Printf(" ")
+		}
+	}
+}
+
+func (s scr) Plot(p pxl, bg int) {
+	if p.x >= s.p1.x && p.x <= s.p2.x &&
+	p.y >= s.p1.y && p.y <= s.p2.y {
 		fmt.Printf("\033[%d;%dH\033[48;5;%dm ", p.y, p.x, bg)
 	}
 }
 
-func Line(p1, p2 pxl, color bool, size pxl) {
+func (s scr) Line(p1, p2 pxl, color bool) {
 	if p1.x > p2.x { //make sure p1 has lowest x (is at the left)
 		p1, p2 = p2, p1
 	}
@@ -28,17 +50,17 @@ func Line(p1, p2 pxl, color bool, size pxl) {
 	   ( p2.y < p1.y && p1.y - p2.y > p2.x - p1.x ) {incx = false}
 	if incx {
 		slope := float64(p2.y - p1.y) / float64(p2.x - p1.x)
-		xstart := 1
-		if p1.x > 1 { xstart = p1.x }
-		xend := size.x
-		if p2.x < size.x { xend = p2.x }
+		xstart := s.p1.x
+		if p1.x > s.p1.x { xstart = p1.x }
+		xend := s.p2.x
+		if p2.x < s.p2.x { xend = p2.x }
 		for x := xstart; x <= xend; x += 1 {
 			y := p1.y + int(slope * float64(x - p1.x) + 0.5)
 			p := pxl{x, y}
 			if color {
-				Plot(p, y*6 + 16, size)
+				s.Plot(p, y*6 + 16)
 			} else {
-				Plot(p, 0, size)
+				s.Plot(p, 0)
 			}
 		}
 	} else {
@@ -46,17 +68,17 @@ func Line(p1, p2 pxl, color bool, size pxl) {
 			p1, p2 = p2, p1
 		}
 		slope := float64(p2.x - p1.x) / float64(p2.y - p1.y)
-		ystart := 1
-		if p1.y > 1 { ystart = p1.y }
-		yend := size.y
-		if p2.y < size.y { yend = p2.y }
+		ystart := s.p1.y
+		if p1.y > s.p1.y { ystart = p1.y }
+		yend := s.p2.y
+		if p2.y < s.p2.y { yend = p2.y }
 		for y := ystart; y <= yend; y += 1 {
 			x := p1.x + int(slope * float64(y - p1.y) + 0.5)
 			p := pxl{x, y}
 			if color {
-				Plot(p, y*6 + 16, size)
+				s.Plot(p, y*6 + 16)
 			} else {
-				Plot(p, 0, size)
+				s.Plot(p, 0)
 			}
 		}
 	}
@@ -78,22 +100,19 @@ func Triangle(center pxl, radius float64, angle float64) (pxl, pxl, pxl) {
 }
 
 func main() {
-	size := pxl{50, 34}
-	cleanstr := ""
-	for i := 0; i < size.y; i++ {
-		cleanstr += strings.Repeat(" ", size.x)
-		cleanstr += "\n"
-	}
+	s := scr{pxl{1, 1}, pxl{60, 28}, 0}
+	s.Flush()
+	s = scr{pxl{10, 7}, pxl{40, 22}, 52}
 	orig := pxl{25, 15}
 	radius := 13.0
 	for i := 0.0; ; i += 0.1 {
 		p1, p2, p3 := Triangle(orig, radius, math.Sin(i) + i)
-		Line(p1, p2, true, size)
-		Line(p2, p3, true, size)
-		Line(p3, p1, true, size)
+		s.Line(p1, p2, true)
+		s.Line(p2, p3, true)
+		s.Line(p3, p1, true)
 		fmt.Printf("\033[0;0H")
 		time.Sleep(25 * time.Millisecond)
-		fmt.Printf("%s", cleanstr)
+		s.Flush()
 	}
 	fmt.Printf("\033[0m\n")
 }
