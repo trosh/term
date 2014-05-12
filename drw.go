0432dbdf94adc2442511f944c14dd3932943c440
trosh <jg.trosh@gmail.com>
2014-05-12 02:37:08 +0200

    made package and separated stuff and things

--- drw.go
+++ drw.go
@@ -1,62 +1,61 @@
-package main
+package term
 
 import (
 	"fmt"
-	"time"
 	"math"
 )
 
-type pxl struct {
-	x int
-	y int
+type Pxl struct {
+	X int
+	Y int
 }
 
-type scr struct {
-	p1 pxl
-	p2 pxl
-	bg int
+type Scr struct {
+	P1 Pxl
+	P2 Pxl
+	Bg int
 }
 
-func (s scr) Size() pxl {
-	size := pxl{s.p2.x - s.p1.x, s.p2.y - s.p1.x}
-	if s.p2.x < s.p1.x { size.x = size.x }
-	if s.p2.y < s.p1.y { size.y = size.y }
+func (s Scr) Size() Pxl {
+	size := Pxl{s.P2.X - s.P1.X, s.P2.Y - s.P1.X}
+	if s.P2.X < s.P1.X { size.X = size.X }
+	if s.P2.Y < s.P1.Y { size.Y = size.Y }
 	return size
 }
 
-func (s scr) Flush() {
-	fmt.Printf("\033[48;5;%dm", s.bg)
-	for y := s.p1.y; y <= s.p2.y; y += 1 {
-		fmt.Printf("\033[%d;%dH", y, s.p1.x)
-		for x := s.p1.x; x <= s.p2.x; x += 1 {
+func (s Scr) Flush() {
+	fmt.Printf("\033[48;5;%dm", s.Bg)
+	for y := s.P1.Y; y <= s.P2.Y; y += 1 {
+		fmt.Printf("\033[%d;%dH", y, s.P1.X)
+		for x := s.P1.X; x <= s.P2.X; x += 1 {
 			fmt.Printf(" ")
 		}
 	}
 }
 
-func (s scr) Plot(p pxl, bg int) {
-	if p.x >= s.p1.x && p.x <= s.p2.x &&
-	p.y >= s.p1.y && p.y <= s.p2.y {
-		fmt.Printf("\033[%d;%dH\033[48;5;%dm ", p.y, p.x, bg)
+func (s Scr) Plot(p Pxl, bg int) {
+	if p.X >= s.P1.X && p.X <= s.P2.X &&
+	p.Y >= s.P1.Y && p.Y <= s.P2.Y {
+		fmt.Printf("\033[%d;%dH\033[48;5;%dm ", p.Y, p.X, bg)
 	}
 }
 
-func (s scr) Line(p1, p2 pxl, color bool) {
-	if p1.x > p2.x { //make sure p1 has lowest x (is at the left)
+func (s Scr) Line(p1, p2 Pxl, color bool) {
+	if p1.X > p2.X { //make sure p1 has lowest x (is at the left)
 		p1, p2 = p2, p1
 	}
 	incx := true
-	if ( p2.y > p1.y && p2.y - p1.y > p2.x - p1.x ) ||
-	   ( p2.y < p1.y && p1.y - p2.y > p2.x - p1.x ) {incx = false}
+	if ( p2.Y > p1.Y && p2.Y - p1.Y > p2.X - p1.X ) ||
+	   ( p2.Y < p1.Y && p1.Y - p2.Y > p2.X - p1.X ) {incx = false}
 	if incx {
-		slope := float64(p2.y - p1.y) / float64(p2.x - p1.x)
-		xstart := s.p1.x
-		if p1.x > s.p1.x { xstart = p1.x }
-		xend := s.p2.x
-		if p2.x < s.p2.x { xend = p2.x }
+		slope := float64(p2.Y - p1.Y) / float64(p2.X - p1.X)
+		xstart := s.P1.X
+		if p1.X > s.P1.X { xstart = p1.X }
+		xend := s.P2.X
+		if p2.X < s.P2.X { xend = p2.X }
 		for x := xstart; x <= xend; x += 1 {
-			y := p1.y + int(slope * float64(x - p1.x) + 0.5)
-			p := pxl{x, y}
+			y := p1.Y + int(slope * float64(x - p1.X) + 0.5)
+			p := Pxl{x, y}
 			if color {
 				s.Plot(p, y*6 + 16)
 			} else {
@@ -64,17 +63,17 @@ func (s scr) Line(p1, p2 pxl, color bool) {
 			}
 		}
 	} else {
-		if p1.y > p2.y { //make sure p1 has lowest y (is at the top)
+		if p1.Y > p2.Y { //make sure p1 has lowest y (is at the top)
 			p1, p2 = p2, p1
 		}
-		slope := float64(p2.x - p1.x) / float64(p2.y - p1.y)
-		ystart := s.p1.y
-		if p1.y > s.p1.y { ystart = p1.y }
-		yend := s.p2.y
-		if p2.y < s.p2.y { yend = p2.y }
+		slope := float64(p2.X - p1.X) / float64(p2.Y - p1.Y)
+		ystart := s.P1.Y
+		if p1.Y > s.P1.Y { ystart = p1.Y }
+		yend := s.P2.Y
+		if p2.Y < s.P2.Y { yend = p2.Y }
 		for y := ystart; y <= yend; y += 1 {
-			x := p1.x + int(slope * float64(y - p1.y) + 0.5)
-			p := pxl{x, y}
+			x := p1.X + int(slope * float64(y - p1.Y) + 0.5)
+			p := Pxl{x, y}
 			if color {
 				s.Plot(p, y*6 + 16)
 			} else {
@@ -82,37 +81,4 @@ func (s scr) Line(p1, p2 pxl, color bool) {
 			}
 		}
 	}
-}
-
-func Triangle(center pxl, radius float64, angle float64) (pxl, pxl, pxl) {
-	angle2 := angle + 2 * math.Pi / 3
-	angle3 := angle + 4 * math.Pi / 3
-	p1 := pxl{
-		int(math.Sin(angle)*radius * 1.7) + center.x,
-		int(math.Cos(angle)*radius      ) + center.y}
-	p2 := pxl{
-		int(math.Sin(angle2)*radius * 1.7) + center.x,
-		int(math.Cos(angle2)*radius      ) + center.y}
-	p3 := pxl{
-		int(math.Sin(angle3)*radius * 1.7) + center.x,
-		int(math.Cos(angle3)*radius      ) + center.y}
-	return p1, p2, p3
-}
-
-func main() {
-	s := scr{pxl{1, 1}, pxl{60, 28}, 0}
-	s.Flush()
-	s = scr{pxl{10, 7}, pxl{40, 22}, 52}
-	orig := pxl{25, 15}
-	radius := 13.0
-	for i := 0.0; ; i += 0.1 {
-		p1, p2, p3 := Triangle(orig, radius, math.Sin(i) + i)
-		s.Line(p1, p2, true)
-		s.Line(p2, p3, true)
-		s.Line(p3, p1, true)
-		fmt.Printf("\033[0;0H")
-		time.Sleep(25 * time.Millisecond)
-		s.Flush()
-	}
-	fmt.Printf("\033[0m\n")
 }
---
+++ tri.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math"
+	"github.com/trosh/term-go/term"
+)
+
+func Triangle(center pxl, radius float64, angle float64) (pxl, pxl, pxl) {
+	angle2 := angle + 2 * math.Pi / 3
+	angle3 := angle + 4 * math.Pi / 3
+	p1 := pxl{
+		int(math.Sin(angle)*radius * 1.7) + center.x,
+		int(math.Cos(angle)*radius      ) + center.y}
+	p2 := pxl{
+		int(math.Sin(angle2)*radius * 1.7) + center.x,
+		int(math.Cos(angle2)*radius      ) + center.y}
+	p3 := pxl{
+		int(math.Sin(angle3)*radius * 1.7) + center.x,
+		int(math.Cos(angle3)*radius) + center.y}
+	return p1, p2, p3
+}
+
+func main() {
+	s := scr{pxl{1, 1}, pxl{60, 28}, 0}
+	s.Flush()
+	s = scr{pxl{10, 7}, pxl{40, 22}, 52}
+	orig := pxl{25, 15}
+	radius := 13.0
+	for i := 0.0; ; i += 0.1 {
+		p1, p2, p3 := Triangle(orig, radius, math.Sin(i) + i)
+		s.Line(p1, p2, true)
+		s.Line(p2, p3, true)
+		s.Line(p3, p1, true)
+		fmt.Printf("\033[0;0H")
+		time.Sleep(25 * time.Millisecond)
+		s.Flush()
+	}
+	fmt.Printf("\033[0m\n")
+}
